354b6f51346922ee273e264dbf7aa65d485efe59
André Bittencourt <andr01a0@stud.kea.dk>
2022-02-01 22:06:44 +0100

    0 intro exercises: Objects

--- 0._Intro/1._Intro_Exercises/2.Objects.js
+++ 0._Intro/1._Intro_Exercises/2.Objects.js
@@ -9,11 +9,17 @@ const alienMessage = {
 
 // Log the message 
 
+console.log(alienMessage.message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const andre = {
+	name: "André",
+	age: 32
+};
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -21,6 +27,8 @@ const alienMessage = {
 const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
+
+stackOverflow.isAllowed = true;
 
 // --------------------------------------
 // Exercise 4 - Remove a property 
@@ -31,6 +39,8 @@ const thisSong = {
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute";
 
 // --------------------------------------
 
